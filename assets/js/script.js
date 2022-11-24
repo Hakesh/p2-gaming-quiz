9682f4989397d366736ccd9bfe79cbffce28bfa2
Hakesh <scout816@gmail.com>
2022-11-24 05:38:35 +0000

    Added code to make ending/end scene work, also remaining questions. Some minor styling too.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,31 +5,37 @@ const quizIntro = document.getElementById("quiz-intro");
 const quizContainerElement = document.getElementById("quiz-container")
 const userInfo = document.getElementById("user-info");
 
-
 const questionElement = document.getElementById("quiz-questions");
 const quizButtonsElement = document.getElementById("answer-buttons");
 
+let ending = document.getElementById("ending");
 let playerName = document.getElementById("player-name");
+let failedTimer = document.getElementById("failed-timer");
 let shuffledQuestions, currentQuestionIndex;
 
-// startButton.addEventListener("click", runGame);
+/**
+ * Function that verifies that player name is at least 3 characters long
+ * and gives red error message in the place holder text
+ */
 startButton.addEventListener("click", function() {
 
     let username = document.getElementById("player-name");
     let valid = username.checkValidity();
     if (valid) {
         runGame();
-        console.log(valid);
     }
     else {
-        // alert("Invalid Username");
         let usernameError = document.getElementById("player-name");
         usernameError.setAttribute("placeholder", "Minimum: 3 Characters")
         username.classList.add("username-error")
         username.value = "";
-        console.log(valid);
     }
 });
+
+
+/**
+ * 
+ */
 resetButton.addEventListener("click", resetGame);
 nextButton.addEventListener("click", () => {
     currentQuestionIndex++;
@@ -37,6 +43,9 @@ nextButton.addEventListener("click", () => {
 })
 
 
+/**
+ * 
+ */
 function runGame() {
     resetScore();
     quizIntro.classList.add("hide");
@@ -44,8 +53,6 @@ function runGame() {
 
     document.getElementById("username").innerText = playerName.value;
     userInfo.classList.remove("hide");
-    console.log(username);
-    console.log(playerName.value)
 
     shuffledQuestions = questions.sort(() => Math.random() - .5);
     currentQuestionIndex = 0;
@@ -53,6 +60,10 @@ function runGame() {
     nextQuestion();
 }
 
+
+/**
+ * 
+ */
 function resetState() {
     clearStatusClass(document.body);
     nextButton.classList.add("hide");
@@ -61,12 +72,20 @@ function resetState() {
     }
 }
 
+
+/**
+ * 
+ */
 function nextQuestion() {
     resetState();
     showQuestion(shuffledQuestions[currentQuestionIndex]);
 
 }
 
+
+/**
+ * 
+ */
 function showQuestion(question) {
     questionElement.innerText = question.question;
 
@@ -101,9 +120,7 @@ function selectAnswer(e) {
     }
     else {
         console.log("ran out of questions");
-        // startButton.innerText = "Restart";
-        // startButton.classList.remove("hide");
-        resetButton.classList.remove("hide");
+        endScreen();
     }
 }
 
@@ -125,6 +142,7 @@ function clearStatusClass(element) {
 
 }
 
+//Collects and counts the score from answering the quiz questions correctly
 function incrementScore() {
 
     let oldScore = parseInt(document.getElementById("score").innerText);
@@ -132,30 +150,58 @@ function incrementScore() {
 
 }
 
+// Resets the score counter
 function resetScore() {
     document.getElementById("score").innerText = "0";
 }
 
+
+// Resets the game back to the main menu / first scene and allows you to play again
 function resetGame() {
     quizIntro.classList.remove("hide");
     quizContainerElement.classList.add("hide");
     userInfo.classList.add("hide");
     resetButton.classList.add("hide");
     startButton.classList.remove("hide")
+    ending.classList.add("hide");
 }
 
 
+function endScreen() {
 
+    // Credit for countdown code: Ali in https://stackoverflow.com/questions/1191865/code-for-a-simple-javascript-countdown-timer
+    let count = 5;
+    let counter = setInterval(timer, 1000);
+    userInfo.classList.add("hide");
+    failedTimer.classList.remove("hide");
 
+    function timer() {
+        count = count-1;
+        if(count <= 0) {
+            clearInterval(counter);
+            quizContainerElement.classList.add("hide");
+            userInfo.classList.add("hide");
+            failedTimer.classList.add("hide");
+        
+            ending.classList.remove("hide");
+            resetButton.classList.remove("hide");
 
+            let finalScore = document.getElementById("final-score");
+            finalScore.innerText = document.getElementById("score").innerText;
+        }
+    }
+
+}
+
+// The constant array holding all of the questions and answers
 const questions = [
     {
         question: "Which MMORPG boasted a massive 12 million players during the years 2009-2010?",
         answers: [
             { text: "Runescape", correct: false },
-            { text: "DOTA", correct: false },
+            { text: "Star Wars: The Old Republic", correct: false },
             { text: "World of Warcraft", correct: true },
-            { text: "League of Legends", correct: false }
+            { text: "Guild Wars", correct: false }
         ]
     },
     {
@@ -180,18 +226,63 @@ const questions = [
         question: "Zug Zug?",
         answers: [
             { text: "Zug Zug", correct: true },
-            { text: "Yes", correct: false },
+            { text: "Kek", correct: false },
             { text: "No?", correct: false },
             { text: "???", correct: false }
         ]
     },
     {
-        question: "What publisher owns the well-known game 'Call of Duty'?",
+        question: "How many classes can you play in the Destiny franchise?",
+        answers: [
+            { text: "1 Class", correct: false },
+            { text: "3 Classes", correct: true },
+            { text: "4 Classes", correct: false },
+            { text: "6 Classes", correct: false }
+        ]
+    },
+    {
+        question: "Which publisher owns the well-known game 'Call of Duty'?",
         answers: [
             { text: "Microsoft", correct: false },
             { text: "Activision", correct: true },
             { text: "Sony", correct: false },
             { text: "Tencent", correct: false }
+        ]
+    },
+    {
+        question: "What is the theme color for Sony's Playstation Consoles?",
+        answers: [
+            { text: "Green", correct: false },
+            { text: "Purple", correct: false },
+            { text: "Brown", correct: false },
+            { text: "Blue", correct: true }
+        ]
+    },
+    {
+        question: "What kind of game is CSGO?",
+        answers: [
+            { text: "RPG", correct: false },
+            { text: "FPS", correct: true },
+            { text: "MMORPG", correct: false },
+            { text: "Racing", correct: false }
+        ]
+    },
+    {
+        question: "What FPS franchise is Sweden well known for?",
+        answers: [
+            { text: "FPSRussia", correct: false },
+            { text: "Apex Legends", correct: false },
+            { text: "Battlefield", correct: true },
+            { text: "PUBG", correct: false }
+        ]
+    },
+    {
+        question: "Which is the most popular streaming platform for games?",
+        answers: [
+            { text: "Youtube", correct: false },
+            { text: "Twitch", correct: true },
+            { text: "Mixer", correct: false },
+            { text: "Facebook Live", correct: false }
         ]
     }
 ]
